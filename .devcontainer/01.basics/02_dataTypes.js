9377e0daf00c8f6cc510b1e200c80485dd3575fe
Shikhar-Tripathi24 <st9358149@gmail.com>
2026-09-15 17:21:58 +0000

    datatypes

---
+++ .devcontainer/01.basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all js code as newer version
+
+// alert(3 +3 ) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3) // code readability shold be high
+
+console.log("Shikhar")    
+
+
+let name = "shikhar"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// underfined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "18");
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
