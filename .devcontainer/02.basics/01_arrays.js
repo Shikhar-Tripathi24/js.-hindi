d5c59551f517e69b2934494ef5f9a420927a25b4
Shikhar-Tripathi24 <st9358149@gmail.com>
2026-09-24 17:21:20 +0000

    arrays

---
+++ .devcontainer/02.basics/01_arrays.js
@@ -0,0 +1,43 @@
+//   arrays //
+
+const myArr = [0,1,2,3,4,5,] 
+const myheroes = ["shaktimaan","nagaraj","dr.jhataka"]
+
+const myArr2 =new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+// Array method //
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop(6)
+
+// myArr.unshift(11)
+// myArr.unshift(11)
+
+// console.log(myArr.includes(34));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr); 
+
+
+// slice, splice
+
+console.log("A". myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B"  ,myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C"  ,myArr);
+console.log(myn2);
+
+
+
+
+
